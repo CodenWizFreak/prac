70d2c94f73569f579a7ea76f094bf59b8093e553
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-03-25 12:34:48 +0530

    Update unstop_scrape.rb

--- unstop_scrape.rb
+++ unstop_scrape.rb
@@ -10,3 +10,11 @@ html_content = URI.open(url)
 # Parse the HTML content using Nokogiri
 doc = Nokogiri::HTML(html_content)
 
+# Example: Extract hackathon titles and links
+hackathons = doc.css('a[href^="/hackathons/"]') # CSS selector for hackathon links
+puts "Unstop Hackathons:"
+hackathons.each_with_index do |hackathon, index|
+  title = hackathon.text.strip
+  link = 'https://unstop.com' + hackathon['href']
+  puts "#{index + 1}. #{title} - #{link}"
+end
